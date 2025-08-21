ffa18098d77555eacc2db10a514ae9e2d7672204
Amit Sharma <168695296+AmitSharma-Code@users.noreply.github.com>
2025-08-21 11:30:57 +0530

    Create Wordpress Function.php

---
+++ Wordpress Function.php
@@ -0,0 +1,13 @@
+woocommerce shop page te banner image
+
+// Add banner image on WooCommerce Shop Page
+add_action( 'woocommerce_before_main_content', 'custom_shop_banner', 5 );
+function custom_shop_banner() {
+    if ( is_shop() ) {
+        echo '<div class="shop-banner">';
+        echo '<img src="https://localseodevelopers.com/bysache/wp-content/uploads/2025/08/New-Project-12.jpg" alt="Shop Banner">';
+        echo '</div>';
+    }
+}
+
+
